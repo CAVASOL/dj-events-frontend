8a01eac8fd1717a7c1dbe2e7ec05214ee24d0c2c
Yeon Kim <awyeon@gmail.com>
2022-08-29 15:39:14 +0800

    connecting to strapi api

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "418a64910ae78058"; }
+/******/ 		__webpack_require__.h = function() { return "1269fc16154a8552"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function HomePage({ events }) {
       {events.length === 0 && <h3>No events to show</h3>}
 
       {events.map((evt) => (
-        <EventItem key={evt.id} evt={evt} />
+        <EventItem key={evt.id} evt={evt.attributes} />
       ))}
 
       {events.length > 0 && (
@@ -24,11 +24,13 @@ export default function HomePage({ events }) {
 }
 
 export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/api/events`);
+  const res = await fetch(
+    `${API_URL}/api/events?_sort=date:ASC&_pagination[limit]=3&populate=*`
+  );
   const events = await res.json();
 
   return {
-    props: { events: events.slice(0, 3) },
+    props: { events: events.data },
     revalidate: 1,
   };
 }
